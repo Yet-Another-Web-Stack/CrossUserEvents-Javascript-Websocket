d93060a19afc28a15cae95ed28922f55b42d8165
Björn Büttner <Idrinth@users.noreply.github.com>
2017-04-13 15:28:15 +0200

    Update yawsSocketConnect.js

--- test/yawsSocketConnect.js
+++ test/yawsSocketConnect.js
@@ -13,7 +13,31 @@ describe('yaws.socketConnect', function() {
   it('yaws.socketConnect() should throw an error on missing argument', function() {
     expect(window.yaws.socketConnect).to.throw("The first parameter 'onMessage' is required to be a function accepting a single parameter of type Blob.");
   });
+  it('yaws.socketConnect() should throw an error on missing argument', function() {
+    expect(window.yaws.socketConnect(function(blob){},{},'/')).to.throw("No Websocket Implementation found");
+  });
+  //basic socket
+  window.Websocket = function(){};
   it('yaws.socketConnect() should be an object', function() {
     window.yaws.socketConnect(function(blob){},{},'/').should.be.a('object');
+  });
+  it('yaws.socketConnect() should be an object', function() {
+    window.yaws.socketConnect(function(blob){},{},'/').should.be.a(window.Websocket);
+  });
+  //reconnecting socket
+  window.ReconnectingWebSocket = function(){};
+  it('yaws.socketConnect() should be an object', function() {
+    window.yaws.socketConnect(function(blob){},{},'/').should.be.a('object');
+  });
+  it('yaws.socketConnect() should be an object', function() {
+    window.yaws.socketConnect(function(blob){},{},'/').should.be.a(window.ReconnectingWebSocket);
+  });
+  //robust socket
+  window.RobustWebSocket = function(){};
+  it('yaws.socketConnect() should be an object', function() {
+    window.yaws.socketConnect(function(blob){},{},'/').should.be.a('object');
+  });
+  it('yaws.socketConnect() should be an object', function() {
+    window.yaws.socketConnect(function(blob){},{},'/').should.be.a(window.RobustWebSocket);
   });
 });
