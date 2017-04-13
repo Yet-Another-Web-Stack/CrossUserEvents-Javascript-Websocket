3a5ef6867b0ed0f2a2faa780f50f7fc2f24e5099
Björn Büttner <Idrinth@users.noreply.github.com>
2017-04-13 15:43:20 +0200

    Update yawsSocketConnect.js

--- test/yawsSocketConnect.js
+++ test/yawsSocketConnect.js
@@ -14,7 +14,7 @@ describe('yaws.socketConnect', function() {
     expect(window.yaws.socketConnect).to.throw("The first parameter 'onMessage' is required to be a function accepting a single parameter of type Blob.");
   });
   it('yaws.socketConnect() should throw an error on missing Websocket', function() {
-    expect(window.yaws.socketConnect(function(blob){},{},'/')).to.throw("No Websocket Implementation found");
+    expect(function(){window.yaws.socketConnect(function(blob){},{},'/')}).to.throw("No Websocket Implementation found");
   });
   //basic socket
   window.Websocket = function(){};
