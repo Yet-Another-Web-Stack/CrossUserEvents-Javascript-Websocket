d2d6b873f6ad57213e93a59ed5cdbe3d7117f2ac
Björn Büttner <Idrinth@users.noreply.github.com>
2017-04-13 14:57:24 +0200

    Update yawsSocketConnect.js

--- test/yawsSocketConnect.js
+++ test/yawsSocketConnect.js
@@ -4,7 +4,7 @@ describe('yaws.socketConnect', function() {
   window=window?window:{};
   require('../src/yawsSocketConnect.js');
   it('yaws should be an object', function() {
-    window.yaws.should.be.a('function');
+    window.yaws.should.be.a('object');
   });
   it('yaws.socketConnect should be a function', function() {
     window.yaws.socketConnect.should.be.a('function');
