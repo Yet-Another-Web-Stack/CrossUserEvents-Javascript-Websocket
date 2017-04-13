f4ed9ff6cb04dec8295bc13625a19ecceb5ddd8f
Björn Büttner <Idrinth@users.noreply.github.com>
2017-04-13 15:19:23 +0200

    Update yawsSocketConnect.js

--- test/yawsSocketConnect.js
+++ test/yawsSocketConnect.js
@@ -11,7 +11,7 @@ describe('yaws.socketConnect', function() {
     window.yaws.socketConnect.should.be.a('function');
   });
   it('yaws.socketConnect() should throw an error on missing argument', function() {
-    expect(window.yaws.socketConnect).to.throw(Error);
+    expect(window.yaws.socketConnect).to.throw("The first parameter 'onMessage' is required to be a function accepting a single parameter of type Blob.");
   });
   it('yaws.socketConnect() should be an object', function() {
     window.yaws.socketConnect(function(blob){},{},'/').should.be.a('object');
